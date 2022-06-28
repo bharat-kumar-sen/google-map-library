942c8a691f5532c81f655113091ad93be901d024
AleykaGazdhar <gazdharaleyka01@gmail.com>
2022-06-28 18:32:43 +0530

    update in signup

---
+++ src/app/views/signup/modules/signup.model.ts
@@ -0,0 +1,9 @@
+export class signupUser {
+  firstName: string = '';
+  lastName: string = '';
+  email: string = '';
+  contact: string = '';
+  password: string = '';
+  repeat: string = '';
+  gender: string = 'male';
+}
--- src/app/views/signup/signup.component.ts
+++ src/app/views/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { signupUser } from './modules/signup.model';
 
 @Component({
   selector: 'app-signup',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  signup: signupUser = new signupUser();
 
   constructor() { }
 
