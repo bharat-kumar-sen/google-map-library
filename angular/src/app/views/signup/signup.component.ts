8cc235e7ce6906ce0460659c5f3efb59ad7c23ff
AleykaGazdhar <gazdharaleyka01@gmail.com>
2022-06-30 11:12:53 +0530

    Validation Signup

--- angular/src/app/views/signup/signup.component.ts
+++ angular/src/app/views/signup/signup.component.ts
@@ -25,43 +25,70 @@ export class SignupComponent implements OnInit {
   }
 
   userFormValidation() {
-    this.signupForm = this.fb.group({
-      firstname: ['', [Validators.required,
-                       Validators.minLength(5),
-                       Validators.pattern('^[a-zA-Z]+$'),
-
-      ]],
-      lastname: ['', [Validators.required,
-                      Validators.minLength(5),
-                      Validators.pattern('^[a-zA-Z]+$'),
-      ]],
-      email: ['', [Validators.required,
-                   Validators.email,
-                   Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
-      ]],
-      phonenumber: ['', [Validators.required,
-                         Validators.minLength(3),
-                         Validators.maxLength(10),
-                         Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$'),
-
-      ]],
-      password: ['', [Validators.required,
-                      Validators.minLength(8),
-                      Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*[^A-Za-z0-9])(?=.*?[0-9]).{8,}$')
-      ]],
-      confirmpassword: ['', [Validators.required,
-                            Validators.minLength(8)
-      ]],
-      gender: 'male',
-    },
-    {
-      Validator : this.configPassword,
-    }
+    this.signupForm = this.fb.group(
+      {
+        firstname: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(5),
+            Validators.pattern('^[a-zA-Z]+$'),
+          ],
+        ],
+        lastname: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(5),
+            Validators.pattern('^[a-zA-Z]+$'),
+          ],
+        ],
+        email: [
+          '',
+          [
+            Validators.required,
+            Validators.email,
+            Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
+          ],
+        ],
+        phonenumber: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(10),
+            Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$'),
+          ],
+        ],
+        password: [
+          '',
+          Validators.compose([
+            Validators.required,
+            Validators.minLength(8),
+            Validators.pattern(
+              '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*[^A-Za-z0-9])(?=.*?[0-9]).{8,}$'
+            ),
+          ]),
+        ],
+        confirmpassword: ['', [Validators.required]],
+        gender: 'male',
+      },
+      {
+        validator: this.passwordConfirming,
+      }
     );
   }
-  configPassword(a: any) {
-    if(a.get('password').value != a.get('confirmpassword').value) {
+  /*   configPassword(a: any) {
+    if(a.get('password').value !== a.get('confirmpassword').value) {
       return {invalid: true };
+    } else {
+      return true;
+    }
+  } */
+
+  passwordConfirming(c: any) {
+    if (c.get('password').value !== c.get('confirmpassword').value) {
+      return { invalid: true };
     } else {
       return true;
     }
@@ -72,6 +99,18 @@ export class SignupComponent implements OnInit {
   }
   onsubmit() {
     console.log(this.signupForm.value);
-    this.signupForm.reset();
   }
+  /* resetForm() {
+    this.userFormValidation();
+    setTimeout(() => {
+      this.signupForm.valu = this.signupForm.reset({
+        firstname: 'Aleyka',
+        lastname: 'gazdhar',
+        eamil: 'abc@gmail.com',
+        password: 'Abc@123',
+        confirmpassword: 'Abc@123',
+        gender: '',
+      });
+    }, 5000);
+  } */
 }
