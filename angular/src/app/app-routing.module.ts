a3c96e789e5664336982dc70443f52c5ae2332e6
shireen <shireen@amwebtech.com>
2022-07-01 15:51:35 +0530

    dropdown added

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'bharat/show-map-marker', pathMatch: 'full' },
+  { path: '', redirectTo: 'bharat/add-map-marker', pathMatch: 'full' },
   {
     path: 'bharat',
     loadChildren: () => import('./views/bharat/b-g-map/b-g-map.module').then((mod) => mod.BGMapModule)
