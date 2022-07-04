bd6d345f2eec9269452cd69188fa6c7eb0b64caa
shireen <shireen@amwebtech.com>
2022-07-04 14:14:30 +0530

    multiple matkers add by image

--- angular/src/assets/js/s-map.js
+++ angular/src/assets/js/s-map.js
@@ -2,54 +2,80 @@ var liveUser = 0;
 var liveUser2x = 0;
 
 var MapObj, map, map2x, zoom = 6;
+
+// Data for the markers consisting of a name, a LatLng and a zIndex for the
+// order in which these markers should display on top of each other.
 var locations = [
-  ['Indore', 22.7196, 75.8577, 4],
-  ['Bhopal', 23.2599, 77.4126, 5],
-  ['Raisen', 23.3327, 77.7824, 5],
-  ['Ujjain', 23.1765, 75.7885, 3],
-  ['Dewas', 22.9676, 76.0534, 2],
-  ['Shajapur', 23.4273, 76.2730, 2],
-  ['Jabalpur', 23.1815, 79.9864, 1],
-  ['Raipur', 21.250000, 81.629997, 1],
-  ['Mumbai', 19.0760, 72.8777],
-  ['Pune', 18.5204, 73.8567],
-  ['Agra', 27.1767, 78.0081,2],
-  ['Delhi', 28.7041, 77.1025,1],
-  ['Jaipur',26.9124, 75.7873],
-  ['Bengaluru',12.9716, 77.5946],
-
+  ['Indore', 22.7196, 75.8577, 'assets/images/indore.jpg', 14],
+  ['Bhopal', 23.2599, 77.4126, 'assets/images/bhopal.jpg', 13],
+  ['Raisen', 23.3327, 77.7824, 'assets/images/raisen.jpg', 12],
+  ['Ujjain', 23.1765, 75.7885, 'assets/images/ujjain.jpg', 11],
+  ['Dewas', 22.9676, 76.0534, 'assets/images/dewas.jpg', 10],
+  ['Shajapur', 23.4273, 76.2730, 'assets/images/shajapur.jpg', 9],
+  ['Jabalpur', 23.1815, 79.9864, 'assets/images/jabalpur.jpg', 8],
+  ['Raipur', 21.250000, 81.629997, 'assets/images/raipur.jpg', 7],
+  ['Mumbai', 19.0760, 72.8777, 'assets/images/mumbai.jpg', 6],
+  ['Pune', 18.5204, 73.8567, 'assets/images/pune.jpg', 5],
+  ['Agra', 27.1767, 78.0081, 'assets/images/agra.jpg', 4],
+  ['Delhi', 28.7041, 77.1025, 'assets/images/delhi.jpg', 3],
+  ['Jaipur', 26.9124, 75.7873, 'assets/images/jaipur.jpg', 2],
+  ['Bengaluru', 12.9716, 77.5946, 'assets/images/banglore.jpg', 1],
 ];
-
 
 function sinitializeMAP() {
   if (google.maps) {
     map = new google.maps.Map(document.getElementById('google_map'), {
       // styles: mapStyles,
       zoom: zoom,
-      center: new google.maps.LatLng(22.7196, 75.8577),
+      center: new google.maps.LatLng(22.7196, 75.8577),// The marker, positioned at indore
       mapTypeId: google.maps.MapTypeId.ROADMAP
-      /*       mapTypeId: google.maps.MapTypeId.TERRAIN,
+      /*mapTypeId: google.maps.MapTypeId.TERRAIN,
             mapTypeControl: false */
     });
-    // The marker, positioned at indore
+    setMarkers(map);
+  }
+}
 
-    var infowindow = new google.maps.InfoWindow();
 
-    var marker, i;
+function setMarkers(map) {
 
-    for (i = 0; i < locations.length; i++) {
-      marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-        map: map
-      });
+  // Display multiple markers on a map
+  var infowindow = new google.maps.InfoWindow(), marker, i;
 
-      google.maps.event.addListener(marker, 'click', (function (marker, i) {
-        return function () {
-          infowindow.setContent(locations[i][0]);
-          infowindow.open(map, marker);
-        }
-      })(marker, i));
-    }
+  // var iconBase = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+
+  const shape = {
+    coords: [1, 1, 1, 20, 18, 20, 18, 1],
+    type: "poly",
+  };
+
+  let image = {
+    url: "",
+    size: new google.maps.Size(71, 71),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(17, 34),
+    scaledSize: new google.maps.Size(25, 25)
+  };
+
+  // Loop through our array of markers & place each one on the map
+  for (i = 0; i < locations.length; i++) {
+    image.url = locations[i][3]
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+      map: map,
+      title: locations[i][0],
+      // shape: shape,
+      // icon: locations[i][3],
+      icon: image,
+    });
+
+    // Each marker to have an info window,This event listener calls addMarker() when the map is clicked.
+    google.maps.event.addListener(marker, 'click', (function (marker, i) {
+      return function () {
+        infowindow.setContent(locations[i][0]);
+        infowindow.open(map, marker);
+      }
+    })(marker, i));
   }
 }
 
